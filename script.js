9b28a2131c30de928687c8a59f8e6e4edadf2b13
Davi do Couto <coutodavi530@gmail.com>
2026-03-28 19:08:50 -0300

    Fazendo o menu hamburguer para celular.

---
+++ script.js
@@ -0,0 +1,13 @@
+let menuIcon = document.querySelector('#menu-icon');
+let ul = document.querySelector('.nav-list');
+let ip = "http://10.26.45.42:5000/api/v1";
+
+menuIcon.addEventListener('click', () => {
+    if (ul.classList.contains('ativo')) {
+        ul.classList.remove('ativo');
+        document.querySelector('#menu-icon img').src = 'img/menu-aberto.png'
+    } else {
+        ul.classList.add('ativo');
+        document.querySelector('#menu-icon img').src = 'img/close.png';
+    }
+})
