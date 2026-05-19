034a3eb3975c312dea96f0985239003ccdf429d4
Luiz Damas Amorim <luizdamasamorim@gmail.com>
2026-05-19 17:41:57 -0300

    adicionando o header na pagina de planejamentos

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ let ip = "http://10.26.45.42:5000/api/v1";
 menuIcon.addEventListener('click', () => {
     if (ul.classList.contains('ativo')) {
         ul.classList.remove('ativo');
-        document.querySelector('#menu-icon img').src = 'img/menu-aberto.png'
+        document.querySelector('#menu-icon img').src = '/img/menu-aberto.png'
     } else {
         ul.classList.add('ativo');
-        document.querySelector('#menu-icon img').src = 'img/close.png';
+        document.querySelector('#menu-icon img').src = '/img/close.png';
     }
 })
